77f9e87d94c90431aa87d73377291544e0230e10
David Petersen <david@familiep.de>
2018-04-24 16:10:19 +0200

    you can now supply colors to style the indicator

--- src/main/java/de/thkoeln/progresspageindicator/CircleIndicator.kt
+++ src/main/java/de/thkoeln/progresspageindicator/CircleIndicator.kt
@@ -1,48 +1,76 @@
 package de.thkoeln.progresspageindicator
 
 import android.content.Context
-import android.graphics.Color
 import android.graphics.drawable.ShapeDrawable
 import android.graphics.drawable.shapes.OvalShape
 import android.support.constraint.ConstraintLayout
 import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import de.thkoeln.progresspageindicator.R.styleable.*
 import kotlinx.android.synthetic.main.circle_indicator.view.*
+import android.graphics.drawable.ColorDrawable
+import android.util.TypedValue
 
 class CircleIndicator(con : Context, attrs: AttributeSet?) : ConstraintLayout(con, attrs){
 
     constructor(con: Context) : this(con, null)
 
+    private val mainCircle = ShapeDrawable(OvalShape())
+    private val outerStrokeCircle = ShapeDrawable(OvalShape())
+    private val innerStrokeCircle = ShapeDrawable(OvalShape())
+
+    //todo: public
     private var circleSizeInPixel = 0
     private var innerStrokeSizeInPixel = 0
     private var outerStrokeSizeInPixel = 0
+
+    private val defaultInnerStrokeColorRes = R.color.colorPrimary
+    private val defaultOuterStrokeColorRes = R.color.colorAccent
+
+    var fillColor = getBackgroundColor()
+        set(value) {
+            field = value
+            mainCircle.paint.color = value!!
+        }
+
+    var innerStrokeColor = ContextCompat.getColor(con, defaultInnerStrokeColorRes)
+        set(value) {
+            field = value
+            innerStrokeCircle.paint.color = value
+        }
+
+    var outerStrokeColor = ContextCompat.getColor(con, defaultOuterStrokeColorRes)
+        set(value) {
+            field = value
+            outerStrokeCircle.paint.color = value
+        }
 
     init{
         LayoutInflater.from(con).inflate(R.layout.circle_indicator, this)
 
         attrs?.let {
-            val attributes = con.theme.obtainStyledAttributes(attrs, R.styleable.CircleIndicator,
+            val a = con.theme.obtainStyledAttributes(attrs, R.styleable.CircleIndicator,
                     0 ,0)
 
-            circleSizeInPixel = attributes.getDimensionPixelSize(
-                    R.styleable.CircleIndicator_circle_size, circleSizeInPixel)
+            circleSizeInPixel = a.getDimensionPixelSize(
+                    CircleIndicator_circle_size, circleSizeInPixel)
 
-            innerStrokeSizeInPixel = attributes.getDimensionPixelSize(
-                    R.styleable.CircleIndicator_inner_stroke_size, innerStrokeSizeInPixel)
+            innerStrokeSizeInPixel = a.getDimensionPixelSize(
+                    CircleIndicator_inner_stroke_size, innerStrokeSizeInPixel)
 
-            outerStrokeSizeInPixel  = attributes.getDimensionPixelSize(
-                    R.styleable.CircleIndicator_outer_stroke_size, outerStrokeSizeInPixel )
+            outerStrokeSizeInPixel  = a.getDimensionPixelSize(
+                    CircleIndicator_outer_stroke_size, outerStrokeSizeInPixel )
+
+            fillColor = a.getColor(CircleIndicator_fill_color, fillColor!!)
+            innerStrokeColor = a.getColor(CircleIndicator_inner_stroke_color, innerStrokeColor)
+            outerStrokeColor = a.getColor(CircleIndicator_outer_stroke_color, outerStrokeColor)
         }
-
-        val outerStrokeCircle = ShapeDrawable(OvalShape())
-        val innerStrokeCircle = ShapeDrawable(OvalShape())
 
         if(outerStrokeSizeInPixel == 0){
             outer_circle_stroke.background = null
         }
         else{
-            outerStrokeCircle.paint.color = ContextCompat.getColor(con, R.color.colorAccent)
             outerStrokeCircle.intrinsicHeight = circleSizeInPixel + (outerStrokeSizeInPixel*2)
             outerStrokeCircle.intrinsicWidth = circleSizeInPixel + (outerStrokeSizeInPixel*2)
             outer_circle_stroke.background = outerStrokeCircle
@@ -52,19 +80,29 @@ class CircleIndicator(con : Context, attrs: AttributeSet?) : ConstraintLayout(co
             inner_circle_stroke.background = null
         }
         else{
-            innerStrokeCircle.paint.color = Color.GREEN
             innerStrokeCircle.intrinsicHeight = circleSizeInPixel
             innerStrokeCircle.intrinsicWidth = circleSizeInPixel
             inner_circle_stroke.background = innerStrokeCircle
         }
 
-
-        val mainCircle = ShapeDrawable(OvalShape())
         mainCircle.intrinsicWidth = circleSizeInPixel - (innerStrokeSizeInPixel*2)
         mainCircle.intrinsicHeight = circleSizeInPixel - (innerStrokeSizeInPixel*2)
-        mainCircle.paint.color = ContextCompat.getColor(con, R.color.colorPrimary)
         mainCircle.paint.strokeWidth = innerStrokeSizeInPixel.toFloat()
         main_circle.background = mainCircle
 
     }
+
+    private fun getBackgroundColor() : Int? {
+        val a = TypedValue()
+        context.theme.resolveAttribute(android.R.attr.windowBackground, a, true)
+        if (a.type >= TypedValue.TYPE_FIRST_COLOR_INT && a.type <= TypedValue.TYPE_LAST_COLOR_INT) {
+            // windowBackground is a color
+            return a.data
+        } else {
+            // windowBackground is not a color, probably a drawable
+            val drawable = ContextCompat.getDrawable(context, a.resourceId)
+            if(drawable is ColorDrawable) return drawable.color }
+        return null
+    }
+
 }
