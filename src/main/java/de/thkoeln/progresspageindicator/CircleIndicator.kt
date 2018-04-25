5a4d4accd477fb1e6d6cac189f273cb22377738f
David Petersen <david@familiep.de>
2018-04-25 11:18:58 +0200

    update circle sizes via properties, do not set single colors but visited/unvisited and active ones

--- src/main/java/de/thkoeln/progresspageindicator/CircleIndicator.kt
+++ src/main/java/de/thkoeln/progresspageindicator/CircleIndicator.kt
@@ -1,6 +1,7 @@
 package de.thkoeln.progresspageindicator
 
 import android.content.Context
+import android.graphics.Color
 import android.graphics.drawable.ShapeDrawable
 import android.graphics.drawable.shapes.OvalShape
 import android.support.constraint.ConstraintLayout
@@ -14,37 +15,41 @@ import android.util.TypedValue
 
 class CircleIndicator(con : Context, attrs: AttributeSet?) : ConstraintLayout(con, attrs){
 
+    companion object {
+        private const val DEFAULT_INNER_STROKE_COLOR_RES = android.R.color.darker_gray
+        private const val DEFAULT_OUTER_STROKE_COLOR_RES = R.color.colorAccent
+        private const val DEFAULT_MAIN_COLOR_RES = android.R.color.holo_green_light //TODO
+        private const val DEFAULT_CIRCLE_DP = 14
+        private const val DEFAULT_STROKE_SIZE_DP = 2
+    }
+
     constructor(con: Context) : this(con, null)
 
     private val mainCircle = ShapeDrawable(OvalShape())
     private val outerStrokeCircle = ShapeDrawable(OvalShape())
     private val innerStrokeCircle = ShapeDrawable(OvalShape())
 
-    //todo: public
-    private var circleSizeInPixel = 0
-    private var innerStrokeSizeInPixel = 0
+    var visitedColor = ContextCompat.getColor(con, DEFAULT_MAIN_COLOR_RES)
+    var unvisitedColor = ContextCompat.getColor(con, DEFAULT_INNER_STROKE_COLOR_RES)
+    var activeColor = ContextCompat.getColor(con, DEFAULT_OUTER_STROKE_COLOR_RES)
+
+    var circleSizeInDp : Int = DEFAULT_CIRCLE_DP
+        set(value) { field = value; circleSizeInPixel = getRoundedPixel(value) }
+
+    var innerStrokeSizeInDp : Int = DEFAULT_STROKE_SIZE_DP
+        set(value) { field= value; innerStrokeSizeInPixel = getRoundedPixel(value)}
+
+    var outerStrokeSizeInDp : Int = DEFAULT_STROKE_SIZE_DP
+        set(value) { field = value; outerStrokeSizeInPixel = getRoundedPixel(value) }
+
+    private var circleSizeInPixel = -1
+        set(value) { field = value; updateMainCircle() }
+
+    private var innerStrokeSizeInPixel = -1
+        set(value) { field = value; updateInnerCircle() }
+
     private var outerStrokeSizeInPixel = 0
-
-    private val defaultInnerStrokeColorRes = R.color.colorPrimary
-    private val defaultOuterStrokeColorRes = R.color.colorAccent
-
-    var fillColor = getBackgroundColor()
-        set(value) {
-            field = value
-            mainCircle.paint.color = value!!
-        }
-
-    var innerStrokeColor = ContextCompat.getColor(con, defaultInnerStrokeColorRes)
-        set(value) {
-            field = value
-            innerStrokeCircle.paint.color = value
-        }
-
-    var outerStrokeColor = ContextCompat.getColor(con, defaultOuterStrokeColorRes)
-        set(value) {
-            field = value
-            outerStrokeCircle.paint.color = value
-        }
+        set(value) { field = value; updateOuterCircle() }
 
     init{
         LayoutInflater.from(con).inflate(R.layout.circle_indicator, this)
@@ -62,37 +67,16 @@ class CircleIndicator(con : Context, attrs: AttributeSet?) : ConstraintLayout(co
             outerStrokeSizeInPixel  = a.getDimensionPixelSize(
                     CircleIndicator_outer_stroke_size, outerStrokeSizeInPixel )
 
-            fillColor = a.getColor(CircleIndicator_fill_color, fillColor!!)
-            innerStrokeColor = a.getColor(CircleIndicator_inner_stroke_color, innerStrokeColor)
-            outerStrokeColor = a.getColor(CircleIndicator_outer_stroke_color, outerStrokeColor)
+            visitedColor = a.getColor(CircleIndicator_visited_color, visitedColor)
+            unvisitedColor = a.getColor(CircleIndicator_unvisited_color, unvisitedColor)
+            activeColor = a.getColor(CircleIndicator_active_color, activeColor)
         }
 
-        if(outerStrokeSizeInPixel == 0){
-            outer_circle_stroke.background = null
-        }
-        else{
-            outerStrokeCircle.intrinsicHeight = circleSizeInPixel + (outerStrokeSizeInPixel*2)
-            outerStrokeCircle.intrinsicWidth = circleSizeInPixel + (outerStrokeSizeInPixel*2)
-            outer_circle_stroke.background = outerStrokeCircle
-        }
-
-        if(innerStrokeSizeInPixel == 0){
-            inner_circle_stroke.background = null
-        }
-        else{
-            innerStrokeCircle.intrinsicHeight = circleSizeInPixel
-            innerStrokeCircle.intrinsicWidth = circleSizeInPixel
-            inner_circle_stroke.background = innerStrokeCircle
-        }
-
-        mainCircle.intrinsicWidth = circleSizeInPixel - (innerStrokeSizeInPixel*2)
-        mainCircle.intrinsicHeight = circleSizeInPixel - (innerStrokeSizeInPixel*2)
-        mainCircle.paint.strokeWidth = innerStrokeSizeInPixel.toFloat()
-        main_circle.background = mainCircle
-
+        setVisited()
     }
 
     private fun getBackgroundColor() : Int? {
+        //https://stackoverflow.com/a/14468034/3694345
         val a = TypedValue()
         context.theme.resolveAttribute(android.R.attr.windowBackground, a, true)
         if (a.type >= TypedValue.TYPE_FIRST_COLOR_INT && a.type <= TypedValue.TYPE_LAST_COLOR_INT) {
@@ -105,4 +89,52 @@ class CircleIndicator(con : Context, attrs: AttributeSet?) : ConstraintLayout(co
         return null
     }
 
+    private fun updateMainCircle(){
+        mainCircle.intrinsicWidth = circleSizeInPixel - (innerStrokeSizeInPixel*2)
+        mainCircle.intrinsicHeight = circleSizeInPixel - (innerStrokeSizeInPixel*2)
+        main_circle.background = mainCircle
+    }
+
+    private fun updateInnerCircle() {
+        if(innerStrokeSizeInPixel == 0){
+            inner_circle_stroke.background = null
+        }
+        else{
+            innerStrokeCircle.intrinsicHeight = circleSizeInPixel
+            innerStrokeCircle.intrinsicWidth = circleSizeInPixel
+            inner_circle_stroke.background = innerStrokeCircle
+        }
+        updateMainCircle() //inner circle affects main circle
+    }
+
+    private fun updateOuterCircle(){
+        if(outerStrokeSizeInPixel == 0){
+            outer_circle_stroke.background = null
+        }
+        else{
+            outerStrokeCircle.intrinsicHeight = circleSizeInPixel + (outerStrokeSizeInPixel*2)
+            outerStrokeCircle.intrinsicWidth = circleSizeInPixel + (outerStrokeSizeInPixel*2)
+            outer_circle_stroke.background = outerStrokeCircle
+        }
+    }
+
+    fun setVisited(){
+        //fill green
+        innerStrokeSizeInPixel = 0
+        mainCircle.paint.color = visitedColor
+    }
+
+    fun setUnvisited(){
+        //do not fill + grey stroke
+        innerStrokeSizeInPixel = getRoundedPixel(DEFAULT_STROKE_SIZE_DP)
+        mainCircle.paint.color = getBackgroundColor() ?: Color.WHITE
+        innerStrokeCircle.paint.color = unvisitedColor
+
+    }
+    //todo: setActive / setInactive - advanced indication
+
+    private fun getRoundedPixel(dp: Int) : Int{
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                dp.toFloat(), resources.displayMetrics).toInt()
+    }
 }
